6eb5e8d54e8cec678d0ebee5cae898af5392d1a7
Luca Del Puppo <luca@delpuppo.net>
2021-07-30 15:00:21 +0200

    100% coverage

--- filter.test.js
+++ filter.test.js
@@ -1,62 +1,79 @@
-const data = require('./candidates.json');
-const filter = require('./filter');
+const data = require("./candidates.json");
+const filter = require("./filter");
 
-it('should return only AVAILABLE_IMMEDIATELY candidates with [AVAILABLE_IMMEDIATELY]', () => {
-  const candidates = filter(data, ['AVAILABLE_IMMEDIATELY']);
-
-  expect(candidates.length).toBe(3);
-  expect(candidates[0].name).toBe('Julian Assange');
-  expect(candidates[1].name).toBe('Marc Brown');
-  expect(candidates[2].name).toBe('Maria Bianchi');
-});
-
-it('should return only AVAILABLE_IMMEDIATELY candidates with [AVAILABLE_IMMEDIATELY, FRESH_GRADS]', () => {
-  const candidates = filter(data, ['AVAILABLE_IMMEDIATELY', 'FRESH_GRAD']);
+it("should return only AVAILABLE_IMMEDIATELY candidates with [AVAILABLE_IMMEDIATELY]", () => {
+  const candidates = filter(data, ["AVAILABLE_IMMEDIATELY"]);
 
   expect(candidates.length).toBe(3);
-  expect(candidates[0].name).toBe('Julian Assange');
-  expect(candidates[1].name).toBe('Marc Brown');
-  expect(candidates[2].name).toBe('Maria Bianchi');
+  expect(candidates[0].name).toBe("Julian Assange");
+  expect(candidates[1].name).toBe("Marc Brown");
+  expect(candidates[2].name).toBe("Maria Bianchi");
 });
 
-it('should return only AVAILABLE_IMMEDIATELY candidates with [AVAILABLE_IMMEDIATELY, FRESH_GRADS, JUNIOR]', () => {
-  const candidates = filter(data, ['AVAILABLE_IMMEDIATELY', 'FRESH_GRAD', 'JUNIOR']);
+it("should return only AVAILABLE_IMMEDIATELY candidates with [AVAILABLE_IMMEDIATELY, FRESH_GRADS]", () => {
+  const candidates = filter(data, ["AVAILABLE_IMMEDIATELY", "FRESH_GRAD"]);
 
   expect(candidates.length).toBe(3);
-  expect(candidates[0].name).toBe('Julian Assange');
-  expect(candidates[1].name).toBe('Marc Brown');
-  expect(candidates[2].name).toBe('Maria Bianchi');
+  expect(candidates[0].name).toBe("Julian Assange");
+  expect(candidates[1].name).toBe("Marc Brown");
+  expect(candidates[2].name).toBe("Maria Bianchi");
 });
 
-it('should return only FRESH_GRAD candidates with [FRESH_GRADS, JUNIOR]', () => {
-  const candidates = filter(data, ['FRESH_GRAD', 'JUNIOR']);
+it("should return only AVAILABLE_IMMEDIATELY candidates with [AVAILABLE_IMMEDIATELY, FRESH_GRADS, JUNIOR]", () => {
+  const candidates = filter(data, [
+    "AVAILABLE_IMMEDIATELY",
+    "FRESH_GRAD",
+    "JUNIOR",
+  ]);
+
+  expect(candidates.length).toBe(3);
+  expect(candidates[0].name).toBe("Julian Assange");
+  expect(candidates[1].name).toBe("Marc Brown");
+  expect(candidates[2].name).toBe("Maria Bianchi");
+});
+
+it("should return only FRESH_GRAD candidates with [FRESH_GRADS, JUNIOR]", () => {
+  const candidates = filter(data, ["FRESH_GRAD", "JUNIOR"]);
 
   expect(candidates.length).toBe(4);
-  expect(candidates[0].name).toBe('Julian Assange');
-  expect(candidates[1].name).toBe('Kelly Diamond');
-  expect(candidates[2].name).toBe('Maria Bianchi');
-  expect(candidates[3].name).toBe('Winnie the Pooh');
+  expect(candidates[0].name).toBe("Julian Assange");
+  expect(candidates[1].name).toBe("Kelly Diamond");
+  expect(candidates[2].name).toBe("Maria Bianchi");
+  expect(candidates[3].name).toBe("Winnie the Pooh");
 });
 
-it('should return 2 JUNIOR candidates with [JUNIOR]', () => {
-  const candidates = filter(data, ['JUNIOR']);
+it("should return 2 JUNIOR candidates with [JUNIOR]", () => {
+  const candidates = filter(data, ["JUNIOR"]);
 
   expect(candidates.length).toBe(2);
-  expect(candidates[0].name).toBe('Julian Assange');
-  expect(candidates[1].name).toBe('Marc Brown');
+  expect(candidates[0].name).toBe("Julian Assange");
+  expect(candidates[1].name).toBe("Marc Brown");
 });
 
-it('should return 1 candidate with [JUNIOR, PHP]', () => {
-  const candidates = filter(data, ['JUNIOR', 'PHP']);
+it("should return 1 candidate with [JUNIOR, PHP]", () => {
+  const candidates = filter(data, ["JUNIOR", "PHP"]);
 
   expect(candidates.length).toBe(1);
-  expect(candidates[0].name).toBe('Marc Brown');
+  expect(candidates[0].name).toBe("Marc Brown");
 });
 
-it('should return 5 candidate with [JAVASCRIPT]', () => {
-  const candidates = filter(data, ['JAVASCRIPT']);
+it("should return 5 candidate with [JAVASCRIPT]", () => {
+  const candidates = filter(data, ["JAVASCRIPT"]);
 
   expect(candidates.length).toBe(5);
-  expect(candidates[0].name).toBe('Julian Assange');
-  expect(candidates[2].name).toBe('Donald Obama');
+  expect(candidates[0].name).toBe("Julian Assange");
+  expect(candidates[2].name).toBe("Donald Obama");
+});
+
+it("should return all candidate with 0 filter", () => {
+  const candidates = filter(data, []);
+
+  expect(candidates.length).toBe(data.length);
+});
+
+it("should return 0 candidate if candidate options are undefined", () => {
+  const candidateWithoutOptions = { ...data[data.length - 1], options: null };
+  const candidates = filter([candidateWithoutOptions], ["JAVASCRIPT"]);
+
+  expect(candidates.length).toBe(0);
 });
